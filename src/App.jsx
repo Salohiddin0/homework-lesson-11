d288294cc763495a157464fa5942d496600ac2f4
Your GitHub Username <Your GitHub Email>
2024-12-25 11:51:42 +0500

    add button

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App () {
+  return (
+    <div>
+      <button className="btn btn-primary m-5">Add</button>
+    </div>
+  )
+}
+export default App
